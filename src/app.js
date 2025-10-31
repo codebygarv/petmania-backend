043ba35aac4fe23076e84c404dbfe789df7c370e
Garv Thakral <144711031+codebygarv@users.noreply.github.com>
2025-10-31 16:54:12 +0530

    Garv/dev Feature : cross - origin policy add

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 const connectionToMongoDb = require("./database/connection");
 connectionToMongoDb();
